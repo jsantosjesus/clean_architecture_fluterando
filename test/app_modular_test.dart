38c1322339ddc14d36e1dc31351d3b811705700c
jadson santos <jadsoonbol@gmail.com>
2023-12-06 23:52:37 -0300

    dependencies inject

--- test/app_modular_test.dart
+++ test/app_modular_test.dart
@@ -1,15 +1,38 @@
+import 'dart:convert';
+
 import 'package:clean_architecture_fluterando/app_modular.dart';
+import 'package:clean_architecture_fluterando/modules/search/domain/entities/result_search.dart';
 import 'package:clean_architecture_fluterando/modules/search/domain/usecase/search_by_text.dart';
 import 'package:clean_architecture_fluterando/modules/search/domain/usecase/search_by_text_impl.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'modules/search/mock/result_search_datasource_mock.dart';
+
+class DioMock extends Mock implements Dio {}
 
 void main() {
+  final dio = DioMock();
   Modular.bindModule(AppModular());
 
   test('should retrieve the usecase without error', () {
     final usecase = Modular.get<SearchByText>();
 
     expect(usecase, isA<SearchByTextImpl>());
+  });
+
+  test('should return a resultSearch list', () async {
+    when(() => dio.get(any())).thenAnswer((_) async => Response(
+        data: jsonDecode(dataMock),
+        statusCode: 200,
+        requestOptions: RequestOptions()));
+
+    final usecase = Modular.get<SearchByText>();
+
+    final result = await usecase('jsantosjesus');
+
+    expect(result.fold((l) => l, (r) => r), isA<List<ResultSearch>>());
   });
 }
